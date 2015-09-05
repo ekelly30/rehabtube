6886c7d53fde669bf958148ffd50566f7a980762
ekelly30 <ekelly30@qub.ac.uk>
2015-09-05 12:53:00 +0100

    Adding pagination
    The search query now returns 20 results, and there is the ability to jump to next
    and previous search results pages.

--- rehabtube.py
+++ rehabtube.py
@@ -5,8 +5,9 @@ import jinja2
 from apiclient.discovery import build
 from private_config import YOUTUBE_API_KEY
 
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+RESULTS_PER_PAGE = 20
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),
@@ -29,37 +30,57 @@ class SearchHandler(webapp2.RequestHandler):
 
     def get(self):
         """Get the search results page"""
-        videos = self.search_by_keyword(self.request.get('Query'))
-        self.generate_page(videos)
+        response = self.search_by_keyword()
+        page_data = self.generate_page_data(response)
+        self.generate_page(page_data)
 
-    def search_by_keyword(self, query_term):
+    def search_by_keyword(self):
         """
         Perform the API query
-        :param query_term: The term to search for
-        :return: A list of videos returned by the query
+        :return: The youtube API search response
         """
+        query = self.request.get('query')
+        page = self.request.get('page')
         youtube = build(YOUTUBE_API_SERVICE_NAME,
                         YOUTUBE_API_VERSION,
                         developerKey=YOUTUBE_API_KEY)
-        search_response = youtube.search().list(q=query_term,
-                                                part="id,snippet",
-                                                maxResults=5).execute()
+        search_response = youtube.search().list(q=query,
+                                                part='id,snippet',
+                                                pageToken=page,
+                                                maxResults=RESULTS_PER_PAGE).execute()
+        return search_response
+
+    def generate_page_data(self, search_response):
+        """
+        From the youtube API response, build the dictionary necessary to
+        populate the jinja template
+        :param search_response: The youtube API search response
+        :return: A dictionary containing the videos, prev & next page tokens,
+                 and the original query term
+        """
+        response = {}
+        response['query'] = self.request.get('query')
+        response['prev_page'] = search_response.get('prevPageToken')
+        response['next_page'] = search_response.get('nextPageToken')
 
         videos = []
-        for search_result in search_response.get("items", []):
+        for search_result in search_response.get('items', []):
             videos.append(search_result)
-        return videos
+        response['videos'] = videos
+        return response
 
-    def generate_page(self, videos):
+    def generate_page(self, page_data):
         """
-        Create the search results page from the list of videos
-        :param videos: A list of videos returned by the youtube API
+        Create the search results page from the API search response
+        :param page_data: A dictionary containing a list of videos,
+                        the previous and next page tokens, and the
+                        initial search query
         :return:
         """
-        template_values = {'videos': videos}
         self.response.headers['Content-type'] = 'text/html'
         template = JINJA_ENVIRONMENT.get_template('search.html')
-        self.response.write(template.render(template_values))
+        self.response.write(template.render(page_data))
+
 
 # Run the app with the appropriate page handlers
 app = webapp2.WSGIApplication([('/', MainHandler),
